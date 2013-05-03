8ad35232b90ddd300c64ee0c1b89af07cc4cf6be
Natxo Raga Llorens <natxoraga22@gmail.com>
2013-05-03 12:46:53 +0200

    kfkslfjasjfñagjañsjgñal

--- Texture.cpp
+++ Texture.cpp
@@ -2,6 +2,7 @@
 #include "Texture.h"
 #include <iostream>
 #include <sstream>
+#include "Globals.h"
 
 
 Texture::Texture(void) {}
@@ -44,6 +45,47 @@ bool Texture::load(const char *filename, const char *ext, int type, int wraps, i
 	// Texture loading
 	if (!mipmap) glTexImage2D(GL_TEXTURE_2D, 0, components, widthInPixels, heightInPixels, 0, type, GL_UNSIGNED_BYTE, img->getPixels());
 	else gluBuild2DMipmaps(GL_TEXTURE_2D, components, widthInPixels, heightInPixels, type, GL_UNSIGNED_BYTE, img->getPixels());
+	return true;
+}
+
+bool Texture::getMapPixelsHeight(char *filename, std::vector< std::vector<float> > &pixels, int type)
+{
+	corona::Image* img;
+	int components;
+
+	img = corona::OpenImage(filename);
+	if(type==GL_RGB)
+	{
+		//img = corona::OpenImage(filename,corona::PF_R8G8B8);
+		components = 3;
+	}
+	else if(type==GL_RGBA)
+	{
+		//img = corona::OpenImage(filename,corona::PF_R8G8B8A8);
+		components = 4;
+	}
+	else return false;
+
+	if(img==NULL) return false;
+
+	widthInPixels  = img->getWidth();
+	heightInPixels = img->getHeight();
+
+	srand (3218551);
+	pixels = std::vector< std::vector<float> >(heightInPixels, std::vector<float>(widthInPixels));
+	byte *p = (byte*) img->getPixels();
+	for (int i = 0; i < heightInPixels; i++) {
+		for (int j = 0; j < widthInPixels; j++) {
+			unsigned int red = (unsigned int) *p++;
+			unsigned int green = (unsigned int) *p++;
+			unsigned int blue = (unsigned int) *p++;
+			// skip alpha channel
+			if (components == 4) p++;
+			float maxValue = 256;
+			float value = (red/maxValue)/3 + (green/maxValue)/3 + (blue/maxValue)/3;
+			pixels[i][j] = value;//((float) rand()) / (float) RAND_MAX; // BETWEEN 0 AND 1
+		}
+	}
 	return true;
 }
 
