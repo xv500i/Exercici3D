e2e57633b144bc0af41984183956ae1830b8b4a4
Alex <xv500i@gmail.com>
2013-05-03 12:47:56 +0200

    obstacles mes definits

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -2,17 +2,23 @@
 
 
 Obstacle::Obstacle(void)
+	: GameObject()
 {
+
 }
 
 
 Obstacle::~Obstacle(void)
 {
+
 }
 
 Obstacle::Obstacle(Point3D &center, float height, float radius, ObstacleType type)
+	:height(height), radius(radius), type(type)
 {
-
+	setXPosition(center.getX());
+	setYPosition(center.getY());
+	setZPosition(center.getZ());
 }
 	
 void Obstacle::render() const
--- Obstacle.h
+++ Obstacle.h
@@ -18,5 +18,6 @@ public:
 private:
 	float height;
 	float radius;
+	ObstacleType type;
 };
 
