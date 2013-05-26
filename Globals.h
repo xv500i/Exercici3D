8c0c150f7b3abafc88a31be04c3f901ce569b0e7
Natxo Raga Llorens <natxoraga22@gmail.com>
2013-05-26 16:31:17 +0200

    DEBUG mode (tecla 'M')

--- Globals.h
+++ Globals.h
@@ -1,13 +1,7 @@
 
-/*#include <windows.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <gl/glut.h>*/
-
 #ifndef _GLOBALS_H_
 #define _GLOBALS_H_
 
-bool DEBUG = true;
+extern bool DEBUG;
 
 #endif
